fedab5dfbf9261b79ab7d4e5b408ef60ddd5f290
gtkarthikeyan26 <gtrkarthikeyan@gmail.com>
2025-04-28 14:57:29 +0530

    Changes

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://ps-backend-oxic.onrender.com',
-        changeOrigin: true,
-        secure: false, // 👈 Bypasses SSL verification
+export default defineConfig(({ mode }) => {
+  // Load env file based on mode (e.g., .env, .env.production)
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-  base: '/',
-  build: {
-    outDir: 'dist', // Vite's default build folder
-  },
+    base: '/',
+    build: {
+      outDir: 'dist',
+    },
+  }
 })
